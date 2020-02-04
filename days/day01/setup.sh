942bb8edff22480f423cdb00b06b9e519095b247
Enrique Ortuno <enrique@ortuno.net>
2020-02-04 08:39:53 -0400

    setup.sh updated

--- days/day01/setup.sh
+++ days/day01/setup.sh
@@ -39,11 +39,11 @@ then
     sudo apt-get update
     sudo apt-get install curl
     # ======================================
-    # INSTALLING AND CONFIGURING NODE JS 6.x
+    # INSTALLING AND CONFIGURING NODE JS 10.x
     # ======================================
-    echo "1\. Installing nodejs 6.x"
+    echo "1\. Installing nodejs 10.x"
     echo "..............."
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
     sudo apt-get install -y nodejs
     
     sudo apt-get update
@@ -84,6 +84,11 @@ then
     echo "Your GIT configuration"
     echo "----------------------"
     git config --list
+    
+    #Generate and Install SSH Keys https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+    #ssh-keygen -t rsa -b 4096 -C "enrique.ortuno@incontact.com"
+    #Copy the content to your git store
+    #cat ~/.ssh/id_rsa.pub
 
     # =================
     # INSTALLING DOCKER
@@ -171,7 +176,7 @@ then
     echo 
     echo "1\. Install Docker Compose..."
     echo
-    sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
     echo
     echo "2\. Apply executable permissions to the binary..."
@@ -187,10 +192,10 @@ then
     echo "Installation completed successfully!!!"
 
     # =============
-    # DOCKER CONFIGURATION
+    # CONFIGURATION
     # =============
     clear
-    echo "Configuring Docker Environment"
+    echo "Configuration environment"
     echo "-------------------------"
 
     echo
@@ -198,78 +203,229 @@ then
     echo
     sudo apt-get install build-essential g++
 
-    echo "2\. Changing folder permission and ownership to current user in /home/$SUDO_USER/.npm/"
+    echo
+    echo "2\. Installing grunt and grunt-cli... "
+    echo
+    sudo npm install -g grunt
+    sudo npm install -g grunt-cli
+
+    echo "3\. Changing folder permission and ownership to current user in /home/$SUDO_USER/.npm/"
     sudo chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.npm/
 
     echo
-    echo "3\. Editing docker file... "
+    echo "4\. Installing Yeoman... "
     echo
-    #sudo sed -i '/DOCKER_OPTS="/c\DOCKER_OPTS="--dns 8.88.8.8 --dns 192.168.13.200 --dns 192.168.13.201"' /etc/default/docker
+    sudo npm install -g yo
 
     echo
-    echo "4\. Restarting docker service... "
+    echo "5\. Editing docker file... "
+    echo
+    sudo sed -i '/DOCKER_OPTS="/c\DOCKER_OPTS="--dns 8.88.8.8 --dns 192.168.13.200 --dns 192.168.13.201"' /etc/default/docker
+
+    echo
+    echo "6\. Setting environment variable... "
+    echo
+    export NODE_ENV=development
+    echo $NODE_ENV
+    echo 
+
+    echo
+    echo "7\. Restarting docker service... "
     echo
     sudo service docker restart
     
-    # =============
-    # TOOLS INSTALLATION
-    # =============
-    clear
-    echo "Installing Tools"
-    echo "-------------------------"
-
-	echo
-    echo "1\. Installing MC aka NortonCommander... "
+    echo
+    echo "8\. Installing MC aka NortonCommander... "
     echo
     sudo apt-get install mc
     
-	echo
-    echo "2\. Installing pip "
+    echo
+    echo "9\. pip "
     echo
     sudo apt-get update
     sudo apt-get upgrade
-	sudo -H apt-get install python-pip && sudo -H pip install -U pip
-	
-    echo
-    echo "2\. Installing awscli "
-    echo
-    sudo pip install awscli --force-reinstall --upgrade
+    sudo -H apt-get install python-pip && sudo -H pip install -U pip
     
     echo
-    echo "3\. Installing tmux "
+    echo "10\. Installing awscli "
+    echo
+    sudo pip install awscli --force-reinstall --upgrade
+    clear
+    
+    echo
+    echo "11\. Installing dotnet core for Ubuntu 17.10 based on https://docs.microsoft.com/en-us/dotnet/core/linux-prerequisites?tabs=netcore2x"
+    echo
+    echo "Register the Microsoft Product key as trusted. "
+    #curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    #sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    
+    #echo "Set up the desired version host package feed. In this case Ubuntu 17.10 "
+    #sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-artful-prod artful main" > /etc/apt/sources.list.d/dotnetdev.list'
+    #sudo apt-get update
+    
+    echo "Install .NET Core."
+    #sudo apt-get install dotnet-sdk-2.1.4
+    sudo add-apt-repository universe
+    sudo apt-get install apt-transport-https
+    sudo apt-get update
+    sudo apt-get install dotnet-sdk-2.2=2.2.108-1
+    
+    echo "Install 7z and rar"
+    sudo apt-get install p7zip-full p7zip-rar
+    
+    echo
+    echo "12\. Installing powerline and prompt customizations "
+    echo
+    sudo -H apt-get install python3-pip && sudo -H pip3 install -U pip3
+    sudo -H pip3 install git+git://github.com/Lokaltog/powerline
+    wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+    wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+    sudo mv PowerlineSymbols.otf /usr/share/fonts/
+    fc-cache -vf /usr/share/fonts/
+    sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+    echo "Ready for edit .bash_profile file and add the following lines at end of file..."
+    
+    sudo apt-get install zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo "After install zsh https://askubuntu.com/questions/342299/zsh-is-not-launched-while-opening-a-new-terminal-with-gnome-terminal"
+    
+    #Install Powerline fonts
+    # clone
+    git clone https://github.com/powerline/fonts.git --depth=1
+    # install
+    cd fonts
+    ./install.sh
+    # clean-up a bit
+    cd ..
+    rm -rf fonts
+    
+    #Install Powerline fonts https://github.com/Anthony25/gnome-terminal-colors-solarized
+    sudo apt-get install dconf-cli
+    git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+    cd gnome-terminal-colors-solarized
+    ./install.sh
+    cd ..
+    rm -rf gnome-terminal-colors-solarized
+    
+    wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
+    mv bullet-train.zsh-theme $ZSH/themes
+    #Change to ZSH_THEME="bullet-train"
+    
+    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    #Change to ZSH_THEME="powerlevel9k/powerlevel9k"
+    
+    echo "Installing gconftool or gconf2"
+    sudo apt install gconf2
+    
+    echo "Installing Color Scheme for Gnome Terminal and Pantheon Terminal from: https://github.com/Mayccoll/Gogh"
+    wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
+    
+    
+    sudo apt-get install compizconfig-settings-manager
+    #Please go to: https://askubuntu.com/questions/128649/compiz-using-8-25-cpu-continually
+    
+    echo "After install go to: http://www.omgubuntu.co.uk/2016/10/things-to-do-after-installing-ubuntu-16-10"
+    
+    sudo add-apt-repository ppa:noobslab/macbuntu
+    sudo apt-get update
+    sudo apt-get install macbuntu-os-icons-lts-v8
+    sudo apt-get install macbuntu-os-ithemes-lts-v8
+    
+    echo "Preparing folder for themes, PLEASE READ: https://itsfoss.com/install-themes-ubuntu/"
+    mkdir ~/.themes 
+    mkdir ~/.icons
+    echo "Please goto https://www.gnome-look.org/browse/cat/135/"
+    echo "Download any theme"
+    echo "Extract it into ~/.themes AND VOILA you can change to that theme"
+    
+    echo "Installing Random Wallpaper"
+    echo "After setup you computer please goto: https://extensions.gnome.org/extension/1040/random-wallpaper"
+    echo "and ENABLE it"
+    sudo apt-get install chrome-gnome-shell
+    
+    
+    echo "Installing masalla icons"
+    cd ~/Downloads
+    sudo git clone https://github.com/hayderctee/masalla-icon-theme.git
+    cd masalla-icon-theme
+    sudo ./INSTALL
+    cd ~/
+    
+    echo
+    echo "13\. Installing go "
+    echo "https://tecadmin.net/install-go-on-ubuntu/#"
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    #sudo wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+    sudo wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
+    #sudo tar -zxvf  go1.7.1.linux-amd64.tar.gz -C /usr/local/
+    sudo tar -xvf go1.9.2.linux-amd64.tar.gz -C /usr/local/
+    export PATH=$PATH:/usr/local/go/bin
+    mkdir $HOME/work
+    export GOPATH=$HOME/work
+
+    sudo npm install -g serverless
+    sudo go get -u github.com/remind101/assume-role 
+    
+    aws configure
+    aws configure --profile dev
+    
+    echo
+    echo "14\. Installing tmux "
     echo
     sudo apt-get update
     sudo apt-get install tmux
-   
+    
+    echo "Configuration completed...."
+    clear
+    
     echo
-    echo "4\. Installing OpenJDK 8 JRE/JDK"
+    echo "15\. Installing OpenJDK 8 JRE/JDK"
     echo
     sudo apt install openjdk-8-jre
     
     echo
-    echo "5\. Installing Gnome Tweak"
+    echo "16\. Installing Gnome Tweak"
     echo
     sudo apt install gnome-tweak-tool
     gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+    gsettings set org.gnome.shell.extensions.dash-to-dock scroll-action 'cycle-windows'
     
     echo
-    echo "6\. Installing Net Tools (inclusing netstat)"
+    echo "17\. Installing Net Tools (inclusing netstat)"
     echo
     sudo apt install net-tools
     
     echo
-    echo "7\. Installing HTop an improved tool over top (monitoring resources)"
+    echo "18\. Installing HTop an improved tool over top (monitoring resources)"
     echo
     sudo apt-get install htop
     
     echo
-    echo "8\. Restarting your ubuntu session"
+    echo "19\. Restart your ubuntu session"
     echo
+
+    echo
+    echo "Installed and Configured completed successfully!!!"
+    echo
+    echo "Details Installed"
+    echo "   - Git "
+    echo "   - Docker "
+    echo "   - Docker-Compose"
+    echo
+    echo "Details Configured"
+    echo "   - Pulling Docker Images"
+    echo "   - Additional Configurations"
+    echo
+
+    echo "Please restart your session if it didn't restart automatically before continuing"
     sudo shutdown -r now 
     echo
 else
     echo
-    echo "Please review file content in order to find possible errors"
+    echo "Please run the info.sh file for more details"
 
 fi
 
