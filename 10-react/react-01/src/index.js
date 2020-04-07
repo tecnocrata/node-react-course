2daee7e73888a92f2ad03976a3840800fd862953
Enrique Ortuno <enrique@ortuno.net>
2020-04-07 18:59:10 -0400

    Simple react examples

--- 10-react/react-01/src/index.js
+++ 10-react/react-01/src/index.js
@@ -16,6 +16,19 @@ class App extends Component {
       <div>
         {/* <Hello name={this.state.name} /> */}
         <p>Start editing to see some magic happen :)</p>
+        <div>
+          <h1>Contacts</h1>
+          <ul>
+            <li>
+              <h2>James Nelson</h2>
+              <a href="mailto:james@jamesknelson.com">james@jamesknelson.com</a>
+            </li>
+            <li>
+              <h2>Joe Citizen</h2>
+              <a href="mailto:joe@example.com">joe@example.com</a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
