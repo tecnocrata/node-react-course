33b7fb22a74f7d7cb0c040b7dcd53de3304a867e
Enrique Ortuno <enrique@ortuno.net>
2020-04-12 00:56:33 -0400

    Initial State example added

---
+++ 10-react/react-08/src/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+import "./style.css";
+
+let Score = (props) => {
+  function increaseScore(e) {
+    e.preventDefault();
+    console.log("Increase!");
+  }
+  return (
+    <div>
+      <p>Score is 0 for the {props.name}</p>
+      <button onClick={increaseScore}>+</button>
+      <button>-</button>
+      <button>Reset</button>
+    </div>
+  );
+};
+
+// class Score extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.props = props;
+//   }
+//   increaseScore() {
+//     console.log("Increase!");
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <p>Score is 0 for the {this.props.name}</p>
+//         <button onClick={this.increaseScore}>+</button>
+//         <button>-</button>
+//         <button>Reset</button>
+//       </div>
+//     );
+//   }
+// }
+
+Score.defaultProps = {};
+
+Score.propTypes = {};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Score name="Wolverines" />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
