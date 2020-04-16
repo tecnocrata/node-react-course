43da1c2f673968cf7da4fa961a70be8243471267
Enrique Ortuno <enrique@ortuno.net>
2020-04-15 21:20:29 -0400

    MongoDB additional examples added

---
+++ 09-mongodb/mongodb-00/readme.md
@@ -0,0 +1,5 @@
+Execute
+
+```
+docker-compose -f mongo-stack.yml up --force-recreate --build
+```
--- 09-mongodb/xpress-api-06/index.js
+++ 09-mongodb/xpress-api-06/index.js
@@ -4,13 +4,14 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+var express = require("express"); // call express
+var app = express(); // define our app using express
+var bodyParser = require("body-parser");
 
-var mongoose   = require('mongoose');
+var mongoose = require("mongoose");
 //Pease create your account at https://mlab.com
-mongoose.connect('mongodb://instructor:abc123A@ds235239.mlab.com:35239/tkn-demo');
+//mongoose.connect('mongodb://instructor:abc123A@ds235239.mlab.com:35239/tkn-demo');
+mongoose.connect("mongodb://localhost/tkn-demo");
 
 //var Person = require('./models/person');
 
@@ -19,30 +20,30 @@ mongoose.connect('mongodb://instructor:abc123A@ds235239.mlab.com:35239/tkn-demo'
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080; // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.(middleware)');
-    next(); // make sure we go to the next routes and don't stop here
+router.use(function (req, res, next) {
+  // do logging
+  console.log("Something is happening.(middleware)");
+  next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 //router.get('/', function(req, res) {
-//    res.json({ message: 'hooray! welcome to our api!' });   
+//    res.json({ message: 'hooray! welcome to our api!' });
 //});
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-require('./routes')(app);
+require("./routes")(app);
 //app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log("Magic happens on port " + port);
