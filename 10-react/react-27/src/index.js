445943ce45e69ee8a7a23c11c2939d5dc532c4cc
Enrique Ortuno <enrique@ortuno.net>
2020-04-18 15:57:47 -0400

    Initial complete apps added

--- 10-react/react-27/src/index.js
+++ 10-react/react-27/src/index.js
@@ -1,59 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { render } from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useLocation,
-  useParams,
-} from "react-router-dom";
 
-import { Nav } from "./components/nav";
-
-import "./style.css";
-
-function App() {
-  return (
-    <Router>
-      <Nav />
-
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact/:country" component={Contact} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </Router>
-  );
-}
-
-// function Nav() {
-//   return (
-//     <>
-//       <Link to="/">Home</Link> | <Link to="/about?name=jamis">About</Link> | <Link to="/contact/us">Contact</Link> | <Link to="/arstrast">arstarst</Link>
-//     </>
-//   );
-// }
-
-function Home() {
-  return <p>Home Page Content</p>;
-}
-
-function About() {
-  var params = new URLSearchParams(useLocation().search);
-  var name = params.get("name");
-  return <p>About Page content for {name}</p>;
-}
-
-function Contact() {
-  var params = useParams();
-  return <p>Contact Page content for {params.country}</p>;
-}
-
-function NotFound() {
-  return <p>Oh No! Page not found.</p>;
-}
-
-render(<App />, document.getElementById("root"));
+import flightReducer from './reducers/flightReducer';
+const store = createStore(flightReducer);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
