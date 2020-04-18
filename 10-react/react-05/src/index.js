5e0881fe1314409cce2832693a6f19e6b44746d2
Enrique Ortuno <enrique@ortuno.net>
2020-04-18 12:30:04 -0400

    Small changes

--- 10-react/react-05/src/index.js
+++ 10-react/react-05/src/index.js
@@ -3,19 +3,25 @@ import { render } from "react-dom";
 import "./style.css";
 
 function List(props) {
-  let items = props.names.map(item => <li>Hello {item}</li>);
+  let items = props.names.map((item) => <li>Hello {item}</li>);
   return (
     <div>
       <ul>{items}</ul>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <ul>{props.names.map(item => <li key={item}>Hello {item}</li>)}</ul>
+  //   </div>
+  // );
 }
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "React"
+      name: "React",
     };
   }
 
