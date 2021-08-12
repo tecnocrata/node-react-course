9ac5030ce5713869476d2e93cf675d8ceaf71a4b
enrique.ortuno <enrique@ortuno.net>
2021-08-12 19:04:40 -0400

    webpack html plugin added

--- 10-react/basic-react-03/webpack.config.js
+++ 10-react/basic-react-03/webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
 };
